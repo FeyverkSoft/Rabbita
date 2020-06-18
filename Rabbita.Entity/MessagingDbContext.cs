3fc16e59880265cf71880eff06a7aba569916c7c
peter <petr-niz1@yandex.ru>
2020-06-18 17:04:05 +0500

    rename

---
+++ Rabbita.Entity/MessagingDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Rabbita.Entity
+{
+    public abstract class MessagingDbContext : DbContext
+    {
+        public MessagingDbContext([NotNull] DbContextOptions options) : base(options) { }
+
+        public override async Task<Int32> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            var ce = ChangeTracker.Entries()
+                .Where(_ => _.State == EntityState.Added || _.State == EntityState.Modified);
+
+            await using (var transaction = await Database.BeginTransactionAsync(cancellationToken))
+            {
+                foreach (var entityEntry in ce)
+                {
+                    var entity = entityEntry.Entity;
+                    var eventMemberName = entityEntry.Metadata.GetAnnotation(RabbitaMagicConst.EventMemberName);
+                    var eventsMemberName = entityEntry.Metadata.GetAnnotation(RabbitaMagicConst.EventsMemberName);
+                }
+
+                await base.SaveChangesAsync(cancellationToken);
+                transaction.Commit();
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Rabbita.Entity/RabbitaMagicConst.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Rabbita.Entity
+{
+    internal static class RabbitaMagicConst
+    {
+        public const String EventMemberName = "Rabbita:IEventMemberName";
+        public const String EventsMemberName = "Rabbita:IEventsMemberName";
+
+        public const String CommandMemberName = "Rabbita:ICommandMemberName";
+        public const String CommandsMemberName = "Rabbita:ICommandsMemberName";
+    }
+}
